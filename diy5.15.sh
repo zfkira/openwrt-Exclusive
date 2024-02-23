50f6c8ef21d7a1976d9c7d97ae9723aeb2cacaab
Zhu Fei <zfkira@163.com>
2024-02-23 20:07:37 +0800

    Update diy5.15.sh

--- diy5.15.sh
+++ diy5.15.sh
@@ -43,6 +43,8 @@ git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme
 # find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
 git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/openwrt-passwall
 git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
+git clone https://github.com/firkerword/luci-app-smartdns.git package/luci-app-smartdns
+git clone https://github.com/firkerword/smartdns.git package/smartdns
 # git clone -b luci-smartdns-new-version https://github.com/xiaorouji/openwrt-passwall.git package/passwall
 # git clone https://github.com/firkerword/luci-app-mosdns.git package/mosdns
 # git clone https://github.com/firkerword/luci-app-smartdns.git package/luci-app-smartdns
